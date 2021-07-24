751cb215f22ae7b3851a09d1b0ad0796582cd61f
Mislav Jančić <mislav.jancic@protonmail.com>
2021-07-24 22:15:22 +0200

    Add Service project, rename main folder and solution

---
+++ Zadatak.MiniWebShop/Zadatak.MiniWebShop.Model/Narudzbe/NacinPlacanja.cs
@@ -0,0 +1,8 @@
+﻿namespace Zadatak.MiniWebShop.Model.Narudzbe
+{
+    public class NacinPlacanja
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+    }
+}
---
+++ Zadatak.MiniWebShop/Zadatak.MiniWebShop.Service/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Zadatak.MiniWebShop.Service
+{
+    public class Class1
+    {
+    }
+}
