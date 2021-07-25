0e68c02e130c72aad8f9b9b296afb671df830588
Mislav Jančić <mislav.jancic@protonmail.com>
2021-07-25 17:05:13 +0200

    Repository interface added

---
+++ Zadatak.MiniWebShop/Zadatak.MiniWebShop.Model/Narudzbe/INacinPlacanjaRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak.MiniWebShop.Model.Narudzbe
+{
+    public interface INacinPlacanjaRepository
+    {
+        Task<IEnumerable<NacinPlacanja>> GetAllNacinPlacanjaAsync();
+        Task<NacinPlacanja> GetNacinPlacanjaByIdAsync(int id);
+    }
+}
---
+++ Zadatak.MiniWebShop/Zadatak.MiniWebShop.Model/Proizvodi/IProizvodRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak.MiniWebShop.Model.Proizvodi
+{
+    public interface IProizvodRepository
+    {
+        Task<IEnumerable<Proizvod>> GetAllProizvodAsync();
+        Task<Proizvod> GetProizvodByIdAsync(int id);
+    }
+}
